0a54e7184bc7ef8894afb170707d5aa44d1e69bc
Victor <victor.melocariati@gmail.com>
2020-11-25 11:39:17 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Condicionais_Eleitorado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eleições");
+            Console.WriteLine("Você está elegível para votação?\n");
+
+            Console.WriteLine("Digite o ano Vigente: ");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano de Nascimento: ");
+            int anoNasc = int.Parse(Console.ReadLine());
+
+            int idade= anoAtual - anoNasc;
+
+            if(idade >=16){
+                Console.WriteLine("Você está elegível para votar este ano");
+            }
+            
+            else{
+                Console.WriteLine("Você NÃO está elegível para votar este ano");
+            }
+
+
+        }
+    }
+}
